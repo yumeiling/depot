c2de2739a6a333e28be4c06244b095e124b6c9ba
yumeiling <yumling@mail2.sysu.edu.cn>
2013-07-23 12:01:59 +0800

    add send mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,18 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #-----------modify---------------------------------
+  config.action_mailer.delivery_method = :test
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com" ,
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave" ,
+    password: "secret" ,
+    enable_starttls_auto: true
+}
 end
